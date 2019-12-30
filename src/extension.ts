fa8ed552f5c2c4fd162e4fcc907005d5a9da2044
Sam Wolfson <wolfson@cs.uw.edu>
2019-12-30 15:59:41 -0800

    Make more user-friendly for beginners

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ import {
     processingTaskFilename
 } from './processing-tasks';
 import * as search from './search';
-
+import { createInterface } from 'readline';
 
 function remindAddToPath() {
     return vscode.window.showInformationMessage('Remember to add Processing to your path!', 'Learn More').then((item: string | undefined) => {
@@ -22,14 +22,14 @@ function remindAddToPath() {
 
 function copyFile(source: fs.PathLike, target: fs.PathLike, cb: Function) {
     let cbCalled = false;
-
+    
     function done(err?: Error) {
         if (!cbCalled) {
             cb(err);
             cbCalled = true;
         }
     }
-
+    
     let rd = fs.createReadStream(source);
     rd.on('error', function (err: Error) {
         done(err);
@@ -42,27 +42,27 @@ function copyFile(source: fs.PathLike, target: fs.PathLike, cb: Function) {
         done();
     });
     rd.pipe(wr);
-
+    
 }
 
 function checkIfProjectOpen(callback: Function) {
-    vscode.window.showWorkspaceFolderPick().then((root: vscode.WorkspaceFolder | undefined) => {
-        if (root === undefined) {
-            vscode.window.showErrorMessage('Open project folder first');
-        }
-        else {
-            fs.stat(root.uri.fsPath + '/' + root.name + '.pde', (err, stats) => {
-                if (err && err.code === 'ENOENT') {
-                    // Named file doesn't exist.
-                    vscode.window.showErrorMessage('Create a ' + root.name + '.pde file first!');
-                } else if (err) {
-                    vscode.window.showErrorMessage('When checking if ' + root.name + '.pde exists: ' + err);
-                } else if (stats.isFile()) {
-                    callback(root);
-                }
-            });
-        }
-    });
+    
+    if (vscode.workspace.workspaceFolders === undefined) {
+        vscode.window.showErrorMessage('Open or create a project folder first');
+    } else {
+        let root = vscode.workspace.workspaceFolders[0];
+        
+        fs.stat(root.uri.fsPath + '/' + root.name + '.pde', (err, stats) => {
+            if (err && err.code === 'ENOENT') {
+                // Named file doesn't exist.
+                vscode.window.showErrorMessage('Create a ' + root.name + '.pde file first!');
+            } else if (err) {
+                vscode.window.showErrorMessage('When checking if ' + root.name + '.pde exists: ' + err);
+            } else if (stats.isFile()) {
+                callback(root);
+            }
+        });
+    }
 }
 
 function openDocErrorMessage(str: string) {
@@ -73,17 +73,89 @@ function openDocErrorMessage(str: string) {
     });
 }
 
+function openNewFolder() {
+    vscode.window.showOpenDialog({
+        openLabel: "Select Project Folder",
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false
+    }).then(e => {
+        if (e !== undefined) {
+            vscode.window.showInputBox({
+                placeHolder: "Enter the name of your new project",
+                ignoreFocusOut: true,
+            }).then(f => {
+                console.log(e);
+                console.log(f);
+                if (f !== undefined && f !== "") {
+                    fs.mkdirSync(e[0].path + "/" + f);
+                    let fd = fs.openSync(e[0].path + "/" + f + "/" + f + ".pde", 'w');
+                    fs.closeSync(fd);
+                    
+                    vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(e[0].path + "/" + f));
+                }
+            });
+        }
+    });
+}
+
+function openExistingFolder() {
+    vscode.window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false
+    }).then(e => {
+        if (e !== undefined) {
+            vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(e[0].path));
+        }
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
+    
+    if (vscode.workspace.workspaceFolders !== undefined) {
+        // workspaceFolders is guaranteed to be defined since we checked it above
+        let root = vscode.workspace.workspaceFolders[0];
+        let path = vscode.Uri.file(root.uri.fsPath + '/' + root.name + '.pde');
+        vscode.window.showTextDocument(path);
+    }
 
+    console.log(vscode.workspace.workspaceFolders);
+    
+    let runItem: vscode.StatusBarItem;
+    
+    runItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    runItem.command = 'processing.RunTaskFile';
+    runItem.text = "$(play) Run Project";
+    
+    context.subscriptions.push(runItem);
+    runItem.show();
+    
+    let createItem: vscode.StatusBarItem;
+    
+    vscode.commands.registerCommand('processing.CreateWorkspace', openNewFolder);
+    createItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    createItem.text = "$(add) New Project";
+    createItem.command = "processing.CreateWorkspace";
+    createItem.show();
+    
+    let openItem: vscode.StatusBarItem;
+    
+    vscode.commands.registerCommand('processing.OpenWorkspace', openExistingFolder);
+    openItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    openItem.text = "$(folder-opened) Open Project";
+    openItem.command = 'processing.OpenWorkspace';
+    openItem.show();
+    
     console.log('Processing language extension is now active!');
-
+    
     let create_task_file = vscode.commands.registerCommand('processing.CreateTaskFile', () => {
-
+        
         const pdeTaskFile = path.join(context.extensionPath, processingTaskFilename);
-
+        
         checkIfProjectOpen((root: vscode.WorkspaceFolder) => {
             let taskPath = path.join(root.uri.fsPath, '.vscode');
-
+            
             function copyTaskFile(destination: string) {
                 copyFile(pdeTaskFile, destination, function (err: Error) {
                     if (err) {
@@ -92,7 +164,7 @@ export function activate(context: vscode.ExtensionContext) {
                     remindAddToPath();
                 });
             }
-
+            
             fs.stat(taskPath, (err, stats) => {
                 if (err && err.code === 'ENOENT') {
                     // .vscode doesn't exist, creating it
@@ -105,9 +177,9 @@ export function activate(context: vscode.ExtensionContext) {
                 } else if (err) {
                     vscode.window.showErrorMessage('When checking if .vscode/ exists: ' + err);
                 } else if (stats.isDirectory()) {
-
+                    
                     taskPath = path.join(taskPath, 'tasks.json');
-
+                    
                     fs.stat(taskPath, (err, stats) => {
                         if (err && err.code === 'ENOENT') {
                             // Task file doesn't exist, creating it
@@ -127,7 +199,7 @@ export function activate(context: vscode.ExtensionContext) {
         });
     });
     context.subscriptions.push(create_task_file);
-
+    
     let run_task_file = vscode.commands.registerCommand('processing.RunTaskFile', () => {
         checkIfProjectOpen((root: vscode.WorkspaceFolder) => {
             const cmd = `${processingCommand} ${buildProcessingArgs(root.uri.fsPath).join(' ')}`;
@@ -141,42 +213,42 @@ export function activate(context: vscode.ExtensionContext) {
         });
     });
     context.subscriptions.push(run_task_file);
-
+    
     let open_documentation = vscode.commands.registerCommand('processing.OpenExtensionDocumentation', () => {
         search.openURL('https://github.com/TobiahZ/processing-vscode#processing-for-visual-studio-code');
     });
     context.subscriptions.push(open_documentation);
-
-
+    
+    
     // Open Processing Documentation, when you already have something you want to search selected
     let open_docs = vscode.commands.registerTextEditorCommand('processing.OpenDocs',
-        (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-
-            // selection[0] is the start, and selection[1] is the end
-            let selection = textEditor.selection;
-            if (!selection.isSingleLine) {
-                openDocErrorMessage('Multiple lines selected, please just select a class or function.');
-                return;
-            }
-
-            let range = undefined;
-            if (!selection.isEmpty) {
-                // selection is not empty, get text from it
-                range = new vscode.Range(selection.start, selection.end);
-            } else {
-                // selection is empty, get any word at cursor
-                range = textEditor.document.getWordRangeAtPosition(selection.active);
-            }
-
-            if (range === undefined) {
-                openDocErrorMessage('Nothing is selected. Please select a class, or use \"Search Documentation\" instead!');
-                return;
-            }
-
-            search.openProcessingDocs(textEditor.document.lineAt(range.start.line).text, range.start.character, range.end.character);
-        });
+    (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+        
+        // selection[0] is the start, and selection[1] is the end
+        let selection = textEditor.selection;
+        if (!selection.isSingleLine) {
+            openDocErrorMessage('Multiple lines selected, please just select a class or function.');
+            return;
+        }
+        
+        let range = undefined;
+        if (!selection.isEmpty) {
+            // selection is not empty, get text from it
+            range = new vscode.Range(selection.start, selection.end);
+        } else {
+            // selection is empty, get any word at cursor
+            range = textEditor.document.getWordRangeAtPosition(selection.active);
+        }
+        
+        if (range === undefined) {
+            openDocErrorMessage('Nothing is selected. Please select a class, or use \"Search Documentation\" instead!');
+            return;
+        }
+        
+        search.openProcessingDocs(textEditor.document.lineAt(range.start.line).text, range.start.character, range.end.character);
+    });
     context.subscriptions.push(open_docs);
-
+    
     let searchUnityDocs = vscode.commands.registerCommand('processing.SearchWebsite', () => {
         vscode.window.showInputBox({
             prompt: 'Search Processing Website:'
